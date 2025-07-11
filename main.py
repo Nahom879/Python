0d369f8b3f9cbed8a0e04463df795431af35f9df
Nahom879 <145666461+Nahom879@users.noreply.github.com>
2025-07-11 14:31:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+first_number = int(input("Enter the first number: "))
+operation = input("Enter the operation(+, -, *, /): ")
+second_number = int(input("Enter the second number: "))
+def calculation(first_number, second_number, operation):
+    if operation == "+":
+        return first_number + second_number
+    elif operation == "-":
+        return first_number - second_number
+    elif operation == "*":
+        return first_number * second_number
+    elif operation == "/":
+        if second_number == 0:
+            print("You cannot divide by zero!")
+        else:
+            return first_number / second_number
+    else:
+        print("Invalid input")
+print(calculation(first_number, second_number, operation))
