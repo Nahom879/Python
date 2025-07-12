fe8ab85b4ddb89c59545b5f1bd9de2243b7bc8b3
Nahom879 <145666461+Nahom879@users.noreply.github.com>
2025-07-12 11:08:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-
 input("Welcome to my calculator program.")
 first_number = int(input("Enter the first number: "))
 operation = input("Enter the operation(+, -, *, /): ")
@@ -21,21 +19,3 @@ def calculation(first_number, second_number, operation):
         print("Invalid input")
 
 print(calculation(first_number, second_number, operation))
-
-
-
-input("Welcome to my number guessing game.")
-num = random.randint(1, 10)
-guess = int(input("Enter a number(1-10): "))
-
-while True:
-    def compare(a, b):
-        if a == b:
-            return "Congratulation. You got the number correct."
-        elif a < b:
-            return "Your number is too low."
-        elif a > b:
-            return "Your number is too high"
-        return None
-    print(compare(guess, num))
-    break
