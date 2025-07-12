c1c258c67d03b442da9dd7d48af7528b4c2971f3
Nahom879 <145666461+Nahom879@users.noreply.github.com>
2025-07-12 11:07:56 +0300

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,17 @@
+import random
+
+input("Welcome to my number guessing game.")
+num = random.randint(1, 10)
+guess = int(input("Enter a number(1-10): "))
+
+a = 0
+while a == 0:
+    def compare(a, b):
+        if a == b:
+            return "Congratulation. You got the number correct."
+        elif a < b:
+            return "Your number is too low."
+        elif a > b:
+            return "Your number is too high"
+        return None
+    print(compare(guess, num))
